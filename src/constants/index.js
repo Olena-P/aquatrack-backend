2411e180f59856508d132aed3d17b02202c01bcb
Olena Vasylchuk <73667823+Olena-P@users.noreply.github.com>
2024-07-19 23:16:00 +0300

    add (#4)
    Co-authored-by: ChernyshovVadym <vad14@i.ua>

--- src/constants/index.js
+++ src/constants/index.js
@@ -40,4 +40,9 @@ export const CLOUDINARY = {
   API_SECRET: 'API_SECRET',
 };
 
+export const GOOGLE = {
+  CLIENT_ID: 'GOOGLE_AUTH_CLIENT_ID',
+  CLIENT_SECRET: 'GOOGLE_AUTH_CLIENT_SECRET',
+};
+
 export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
