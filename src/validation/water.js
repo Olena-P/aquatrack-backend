72067194cf6e4c6f0888479b020fdee46c88f3ed
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-07-22 10:24:29 +0300

    fix water schema

--- src/validation/water.js
+++ src/validation/water.js
@@ -4,7 +4,7 @@ const dateRegex = /^((0[1-9]|[1-2][0-9]|3[01])\.(0[1-9]|1[0-2])\.\d{4})$/;
 const timeRegex = /^(?:2[0-4]|[01]?\d):(?:[0-5]\d)$/;
 
 export const createWaterVolumeSchema = Joi.object({
-  volume: Joi.number().positive().min(50).max(500).integer(),
+  volume: Joi.number().positive().integer(),
   date: Joi.string()
     .pattern(dateRegex)
     .message('Date must be dd.mm.yyyy / Where dd: 01-31, mm: 01-12'),
@@ -14,7 +14,7 @@ export const createWaterVolumeSchema = Joi.object({
 }).options({ abortEarly: false });
 
 export const updateWaterVolumeSchema = Joi.object({
-  volume: Joi.number().positive().min(50).max(500).integer(),
+  volume: Joi.number().positive().integer(),
   date: Joi.string()
     .pattern(dateRegex)
     .message('Date must be dd.mm.yyyy / Where dd: 01-31, mm: 01-12'),
