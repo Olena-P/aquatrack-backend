f2c2d07bc904f95086059a06ccc2627420447ce2
valiana35 <144236658+valiana35@users.noreply.github.com>
2024-07-25 12:42:05 +0300

    add-user (#52)

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -34,6 +34,7 @@ export const registerUserController = async (req, res) => {
 };
 
 export const loginUserController = async (req, res) => {
+  const user = await registerUser(req.body);
   const session = await loginUser(req.body);
 
   setupSession(res, session);
@@ -42,6 +43,7 @@ export const loginUserController = async (req, res) => {
     status: 200,
     message: 'Successfully logged in a user!',
     data: {
+      user,
       accessToken: session.accessToken,
     },
   });
